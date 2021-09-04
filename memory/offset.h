d0ecdd0f162750927e1a1cb278dc34c0985cd119
Victor Machado <laarky@Victors-iMac-Pro.local>
2021-09-04 15:27:43 -0300

    Last offsets

--- memory/offset.h
+++ memory/offset.h
@@ -3,11 +3,11 @@
 #include <Windows.h>
 
 namespace offset {
-	const DWORD dwLocalPlayer = 0xD3DD14;
-	const DWORD dwEntityList = 0x4D5239C;
-	const DWORD m_iCrosshairId = 0xB3E4;
+	const DWORD dwLocalPlayer = 0xD8A2DC;
+	const DWORD dwEntityList = 0x4DA31EC;
+	const DWORD m_iCrosshairId = 0xB3E8;
 	const DWORD m_iTeamNum = 0xF4;
 	const DWORD m_iHealth = 0x100;
 	const DWORD s_EntityLoop = 0x10;
-	const DWORD dwForceAttack = 0x3183920;
+	const DWORD dwForceAttack = 0x31D3714;
 }
